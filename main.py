2e79a99f9dec755ebcf28a5c4cfe9c4f056ab108
klgaby440-lang <klgaby440@gmail.com>
2026-08-27 20:58:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ origins = [
     "https://ll-one-self.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://klgaby440-lang.github.io/class-net/",
+    "https://klgaby440-lang.github.io/class-net",
+    "https://class-net-fawn.vercel.app",
 ]
 
 app.add_middleware(
